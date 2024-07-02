7506ab5be2416357c279802e33d9d6e7a0b92c7e
moononournation <moononournation@gmail.com>
2024-07-02 23:39:43 +0800

    ledcSetup before ledcAttachPin

--- examples/esp32-nofrendo/display.cpp
+++ examples/esp32-nofrendo/display.cpp
@@ -70,8 +70,8 @@ extern void display_begin()
 
 #ifdef TFT_BL
     // turn display backlight on
-    ledcAttachPin(TFT_BL, 1);     // assign TFT_BL pin to channel 1
     ledcSetup(1, 12000, 8);       // 12 kHz PWM, 8-bit resolution
+    ledcAttachPin(TFT_BL, 1);     // assign TFT_BL pin to channel 1
     ledcWrite(1, TFT_BRIGHTNESS); // brightness 0 - 255
 #endif
 }
